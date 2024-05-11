0df48c236933dc26ad75b0b617118efba3ece763
Ribhu Das Purkayastha <48654809+rdp23@users.noreply.github.com>
2024-05-11 17:39:31 +0530

    Add initial scaffolding
    1. Add a basic homepage and a custom theme
    
    Signed-off-by: Ribhu Das Purkayastha <48654809+rdp23@users.noreply.github.com>

---
+++ docs/sphinx/docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+
+project = 'Ruminations'
+copyright = '2024, Ribhu Das Purkayastha'
+author = 'Ribhu Das Purkayastha'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
+html_css_files = ["custom.css"]
+
+html_theme_options = {
+    "sidebarbgcolor" : "cce6ff"
+}
+
+
---
+++ docs/sphinx/docs/source/index.rst
@@ -0,0 +1,20 @@
+.. r1bhu_githubio documentation master file, created by
+   sphinx-quickstart on Sat May 11 11:25:05 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Behold
+==========================================
+
+This is where I intend to dump 
+
+* showerthoughts
+* links to rabbit holes that I'd want to dive in later (read: never), and
+* stuff
+
+Occasionally I might also add content mildly relevant to the general engineering community.
+
+.. image:: images/goose.jpg
+  :width: 400
+  :alt: Alternative text
+  :align: center
